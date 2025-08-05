85a8451cba7d9f14ede2b6c38e2c4dcff5c46575
JUANDIEGO6666 <jugonzalezle@unal.edu.co>
2025-08-05 13:30:50 -0500

    introducción a pandas 5%

---
+++ ConceptosPrevios/TallerAnalisis.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Dataset ficticio
+datos_estudiantes = {
+    "peso": pd.Series([55, 68, 74, 60, 72], index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"]),
+    "altura": pd.Series([162, 175, 168, 180, 170], index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"]),
+    "promedio": pd.Series([4.5, 3.8, 4.2, 2.9, 3.5], index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"]),
+    "edad": pd.Series([17, 18, 17, 19, 18], index=["Ana", "Carlos", "Daniela", "Eduardo", "Fernanda"])
+}
+
+df = pd.DataFrame(datos_estudiantes)
+print(df)
